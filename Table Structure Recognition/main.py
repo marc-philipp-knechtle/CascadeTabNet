72416c48af4363198c05046555611561f63b3bdb
Marc-Philipp Knechtle <marc-philipp.knechtle@anevis-solutions.com>
2022-01-23 10:03:38 +0100

    add python conventions + personal paths

--- Table Structure Recognition/main.py
+++ Table Structure Recognition/main.py
@@ -1,21 +1,23 @@
-from border import border
-from mmdet.apis import inference_detector, show_result, init_detector
-import cv2
-from Functions.blessFunc import borderless
-import lxml.etree as etree
 import glob
 
-############ To Do ############
-image_path = 'path to directory of images'
-xmlPath = 'path to save xml'
+import cv2
+import lxml.etree as etree
+from mmdet.apis import inference_detector, init_detector
 
-config_fname = "path to config file of model" 
-checkpoint_path = "path to checkpoint directory"
-epoch = 'epoch_file.name'
-##############################
+from Functions.blessFunc import borderless
+from border import border
 
+SCRIPTS_LOCATION: str = "/home/makn/workspace-uni/CascadeTabNetTests"
+CASCADE_TAB_NET_REPO_LOCATION: str = SCRIPTS_LOCATION + "/CascadeTabNet"
 
-model = init_detector(config_fname, checkpoint_path+epoch)
+# To Do
+image_path = '/home/makn/Downloads/sample-tables/invoice.jpg'
+xmlPath = '/home/makn/Downloads/sample-xml/'
+
+config_fname = CASCADE_TAB_NET_REPO_LOCATION + "/Config/cascade_mask_rcnn_hrnetv2p_w32_20e.py"
+checkpoint_file = SCRIPTS_LOCATION + "/epoch_36.pth"
+
+model = init_detector(config_fname, checkpoint_file)
 
 # List of images in the image_path
 imgs = glob.glob(image_path)
@@ -25,34 +27,34 @@ for i in imgs:
     res_bless = []
     res_cell = []
     root = etree.Element("document")
-    ## for border
+    # for border
     for r in result[0][0]:
-        if r[4]>.85:
+        if r[4] > .85:
             res_border.append(r[:4].astype(int))
-    ## for cells
+    # for cells
     for r in result[0][1]:
-        if r[4]>.85:
-            r[4] = r[4]*100
+        if r[4] > .85:
+            r[4] = r[4] * 100
             res_cell.append(r.astype(int))
-    ## for borderless
+    # for borderless
     for r in result[0][2]:
-        if r[4]>.85:
+        if r[4] > .85:
             res_bless.append(r[:4].astype(int))
 
-    ## if border tables detected 
+    # if border tables detected
     if len(res_border) != 0:
-        ## call border script for each table in image
+        # call border script for each table in image
         for res in res_border:
             try:
-                root.append(border(res,cv2.imread(i)))  
+                root.append(border(res, cv2.imread(i)))
             except:
                 pass
     if len(res_bless) != 0:
         if len(res_cell) != 0:
-            for no,res in enumerate(res_bless):
-                root.append(borderless(res,cv2.imread(i),res_cell))
+            for no, res in enumerate(res_bless):
+                root.append(borderless(res, cv2.imread(i), res_cell))
 
-    myfile = open(xmlPath+i.split('/')[-1][:-3]+'xml', "w")
+    myfile = open(xmlPath + i.split('/')[-1][:-3] + 'xml', "w")
     myfile.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    myfile.write(etree.tostring(root, pretty_print=True,encoding="unicode"))
+    myfile.write(etree.tostring(root, pretty_print=True, encoding="unicode"))
     myfile.close()
