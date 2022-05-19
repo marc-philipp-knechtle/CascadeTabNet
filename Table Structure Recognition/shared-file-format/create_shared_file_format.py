90d0dd9f2b43fed3ec9b7d29d6a5dbf3ad51353d
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2022-05-19 16:50:06 +0200

    reorder cell creation

--- Table Structure Recognition/shared-file-format/create_shared_file_format.py
+++ Table Structure Recognition/shared-file-format/create_shared_file_format.py
@@ -50,7 +50,11 @@ def process_image(image_path: str, config_fname: str, checkpoint_file: str) -> D
     cell_counter: int = 0
     # Polygon creation
     for cell in result_cells_bounding_boxes:
-        cell_polygons.append(PolygonRegion(oid=cell_counter, polygon=cell, region_type="unknown"))
+        # the cell array has a weird format which produces conflicts with other applications in downstream tasks
+        # they produce a cross-like shape for detection
+        # this is the reason the cell list is reordered properly
+        cell_ordered: list = [cell[0], cell[2], cell[1], cell[3]]
+        cell_polygons.append(PolygonRegion(oid=cell_counter, polygon=cell_ordered, region_type="unknown"))
         cell_counter += 1
 
     image: Image = Image.open(image_path)
